e61abd636e0293c52bb49af405ef74ef371fe4fa
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2020-09-07 22:55:38 +0200

    Add test ROM

---
+++ testrom/test.asm
@@ -0,0 +1,72 @@
+.segment "HEADER"
+
+INES_MAPPER = 0
+INES_MIRROR = 1
+INES_SRAM   = 0
+
+.byte 'N', 'E', 'S', $1A ; ID
+.byte $02 ; 16k PRG chunk count
+.byte $01 ; 8k CHR chunk count
+.byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
+.byte (INES_MAPPER & %11110000)
+.byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
+
+.segment "TILES"
+.incbin "testbg.chr"
+.incbin "testspr.chr"
+
+.segment "VECTORS"
+.word nmi
+.word reset
+.word irq
+
+.segment "CODE"
+reset:
+    SEI
+    CLD
+    LDX #$FF
+    TXS
+    LDA #$82
+    ADC #$F0
+    BRK
+
+nmi:
+    RTI
+
+irq:
+    RTI
+
+.segment "ZEROPAGE"
+nmi_lock:       .res 1 ; prevents NMI re-entry
+nmi_count:      .res 1 ; is incremented every NMI
+nmi_ready:      .res 1 ; set to 1 to push a PPU frame update, 2 to turn rendering off next NMI
+nmt_update_len: .res 1 ; number of bytes in nmt_update buffer
+scroll_x:       .res 1 ; x scroll position
+scroll_y:       .res 1 ; y scroll position
+scroll_nmt:     .res 1 ; nametable select (0-3 = $2000,$2400,$2800,$2C00)
+temp:           .res 1 ; temporary variable
+gamepad:        .res 1
+cursor_x: .res 1
+cursor_y: .res 1
+temp_x:   .res 1
+temp_y:   .res 1
+
+
+.segment "BSS"
+nmt_update: .res 256 ; nametable update entry buffer for PPU update
+palette:    .res 32  ; palette buffer for PPU update
+
+.segment "OAM"
+oam: .res 256        ; sprite OAM data to be uploaded by DMA
+
+.segment "RODATA"
+example_palette:
+.byte $0F,$15,$26,$37 ; bg0 purple/pink
+.byte $0F,$09,$19,$29 ; bg1 green
+.byte $0F,$01,$11,$21 ; bg2 blue
+.byte $0F,$00,$10,$30 ; bg3 greyscale
+.byte $0F,$18,$28,$38 ; sp0 yellow
+.byte $0F,$14,$24,$34 ; sp1 purple
+.byte $0F,$1B,$2B,$3B ; sp2 teal
+.byte $0F,$12,$22,$32 ; sp3 marine
+
