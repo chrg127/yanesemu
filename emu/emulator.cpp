84d2ff2b9a25cd0b1f080a7c34bf44dc85dbf67b
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2020-11-02 22:28:32 +0100

    Add an emulator class

---
+++ emu/emulator.cpp
@@ -0,0 +1,48 @@
+#include <emu/emulator.hpp>
+#include <emu/core/memorymap.hpp>
+
+using Utils::File;
+using namespace Core;
+
+void Emulator::init(Cartridge &cart)
+{
+    cpu.power(cart.get_prgrom());
+    cpu.attach_ppu(&ppu);
+    ppu.power(cart.get_chrrom(), 0);
+}
+
+void Emulator::run()
+{
+    cpu.main();
+    int curr_cycle = cpu.get_cycles();
+    int cycle_diff = curr_cycle - cycle;
+    // run 3 ppu cycles for 1 cpu cycle
+    for (int i = 0; i < cycle_diff*3; i++)
+        ppu.main();
+    cycle = curr_cycle;
+}
+
+void Emulator::log(File &logfile)
+{
+    cpu.printinfo(logfile);
+    logfile.printf("Instruction [%02X] ", cpu.peek_opcode());
+    logfile.putstr(cpu.disassemble().c_str());
+    logfile.putc('\n');
+}
+
+void Emulator::dump(File &dumpfile)
+{
+    auto dumpmem = [](File &df, const uint8_t *mem, const std::size_t size) {
+        std::size_t i, j;
+
+        for (i = 0; i < size; i++) {
+            df.printf("%04lX: ", i);
+            for (j = 0; j < 16; j++)
+                df.printf("%02X ", mem[i++]);
+            df.putc('\n');
+        }
+        df.putc('\n');
+    };
+    dumpmem(dumpfile, cpu.getmemory(), CPUMap::MEMSIZE);
+    dumpmem(dumpfile, ppu.getmemory(), PPUMap::MEMSIZE);
+}
---
+++ emu/emulator.hpp
@@ -0,0 +1,21 @@
+#ifndef EMULATOR_HPP_INCLUDED
+#define EMULATOR_HPP_INCLUDED
+
+#include <emu/core/cpu.hpp>
+#include <emu/core/ppu.hpp>
+#include <emu/core/cartridge.hpp>
+#include <emu/utils/file.hpp>
+
+class Emulator {
+    Core::CPU cpu;
+    Core::PPU ppu;
+    int cycle = 0;
+
+public:
+    void init(Core::Cartridge &cart);
+    void run();
+    void log(Utils::File &logfile);
+    void dump(Utils::File &dumpfile);
+};
+
+#endif
