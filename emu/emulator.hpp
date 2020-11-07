d3780092c1c4bea20ff91ac3298c0d4d80f14e02
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2020-11-07 15:37:37 +0100

    Why weren't these two files commited before i don't know

--- emu/emulator.hpp
+++ emu/emulator.hpp
@@ -2,13 +2,13 @@
 #define EMULATOR_HPP_INCLUDED
 
 #include <emu/core/cpu.hpp>
-#include <emu/core/ppu.hpp>
+// #include <emu/core/ppu.hpp>
 #include <emu/core/cartridge.hpp>
 #include <emu/utils/file.hpp>
 
 class Emulator {
     Core::CPU cpu;
-    Core::PPU ppu;
+    // Core::PPU ppu;
     int cycle = 0;
 
 public:
