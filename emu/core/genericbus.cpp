8844686b9d53ab521da3d528a02febc821e80497
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2020-12-02 01:28:22 +0100

    Modify Bus so it used Reader and Writer funcs instead

--- emu/core/genericbus.cpp
+++ emu/core/genericbus.cpp
@@ -1,21 +1,13 @@
 #include "genericbus.hpp"
 
 #include <cstring> // memcpy
-#include <algorithm> // std::copy
 #include <emu/util/stringops.hpp>
 #include <emu/util/debug.hpp>
 
 namespace Core {
 
-Bus::Bus(const std::size_t s)
-{
-    reset(s);
-}
-
 Bus::~Bus()
 {
-    if (mem)
-        delete[] mem;
     if (lookup)
         delete[] lookup;
 }
@@ -23,63 +15,49 @@ Bus::~Bus()
 Bus & Bus::operator=(Bus &&b)
 {
     size   = b.size;
-    mem    = b.mem;
     lookup = b.lookup;
-    for (int i = 0; i < MAX_ADDRESSERS; i++)
-        ad_tab[i] = b.ad_tab[i];
-    std::memcpy(counter, b.counter, MAX_ADDRESSERS);
-    b.mem    = nullptr;
+    for (int i = 0; i < TABSIZ; i++) {
+        rtab[i] = b.rtab[i];
+        wtab[i] = b.wtab[i];
+    }
+    std::memcpy(assigned, b.assigned, TABSIZ);
     b.lookup = nullptr;
     return *this;
 }
 
 void Bus::reset(const std::size_t s)
 {
-    if (mem)
-        delete[] mem;
     if (lookup)
         delete[] lookup;
-    // init all memory to zero
-    mem     = new uint8[s]();
-    lookup  = new unsigned int[s]();
-    size    = s;
-    ad_tab[0] = [](uint16 addr) { return addr; };
-    for (auto &x : counter)
-        x = false;
+    size = s;
+    lookup = new unsigned[size]();
+    std::memset(assigned, 0, TABSIZ);
 }
 
 uint8 Bus::read(const uint16 addr) const
 {
-    return mem[ad_tab[lookup[addr]](addr)];
+    return rtab[lookup[addr]](addr);
 }
 
 void Bus::write(const uint16 addr, const uint8 data)
 {
-    mem[ad_tab[lookup[addr]](addr)] = data;
+    wtab[lookup[addr]](addr, data);
 }
 
-/* Maps area between start-end to addresser. Mapping already mapped areas
- * is allowed; if such errors happen, the new function will simply replace
- * the old one. Only MAX_ADDRESSERS are allowed and the first one is used
- * by a default addresser that simply returns its input. */
-void Bus::map(uint16 start, uint16 end, Addresser addresser)
+void Bus::map(uint16 start, uint16 end, Reader reader, Writer writer)
 {
     int id = 0;
 
-    if (!mem) {
-        error("bus memory not initialized");
-        return;
-    }
     /* search for a new id */
-    while (counter[id]) {
-        if (++id > MAX_ADDRESSERS) {
+    while (assigned[id]) {
+        if (++id > TABSIZ) {
             error("mapping exausted");
             return;
         }
     }
-    counter[id] = true;
-    ad_tab[id] = addresser;
-    for (uint16 i = start; i < end; i++)
+    rtab[id] = reader;
+    wtab[id] = writer;
+    for (uint16 i = start; i != end; i++)
         lookup[i] = id;
 }
 
--- emu/core/genericbus.hpp
+++ emu/core/genericbus.hpp
@@ -10,36 +10,31 @@ namespace Core {
  * It works by using the map() function to bind a memory area to a
  * function which has to convert addresses suitable for addressing. */
 class Bus {
-    static const int MAX_ADDRESSERS = 8;
-    using Addresser = std::function<uint16(uint16)>;
+    static const int TABSIZ = 8;
+    using Reader = std::function<uint8(uint16)>;
+    using Writer = std::function<void(uint16, uint8)>;
 
-    std::size_t     size = 0;
-    uint8           *mem = nullptr;
-    unsigned int    *lookup = nullptr;
-    Addresser       ad_tab[MAX_ADDRESSERS];
-    bool            counter[MAX_ADDRESSERS];
+    std::size_t size = 0;
+    unsigned   *lookup = nullptr;
+    Reader      rtab[TABSIZ];
+    Writer      wtab[TABSIZ];
+    bool        assigned[TABSIZ];
 
 public:
     Bus() = default;
-    Bus(const std::size_t s);
+    Bus(const std::size_t s)
+    { reset(s); }
     Bus(const Bus &) = delete;
     Bus(Bus &&b)
     { operator=(std::move(b)); }
     ~Bus();
-
     Bus & operator=(const Bus &) = delete;
     Bus & operator=(Bus &&b);
 
     void   reset(const std::size_t s);
     uint8  read(const uint16 addr) const;
     void   write(const uint16 addr, const uint8 data);
-    void   map(uint16 start, uint16 end, Addresser addresser);
-    uint8 *memory() const
-    { return mem; }
-    // uint16 test(uint16 addr)
-    // {
-    //     return ad_tab[lookup[addr]](addr);
-    // }
+    void   map(uint16 start, uint16 end, Reader reader, Writer writer);
 };
 
 } // namespace Core
